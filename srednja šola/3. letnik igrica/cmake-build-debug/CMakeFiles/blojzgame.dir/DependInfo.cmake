731c61b103a29ae771d28ab6a3ba523a521e0052
luka hribar <luka.hribar210@gmail.com>
2024-07-09 19:46:15 +0200

    moved to different folder

---
+++ srednja šola/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Animations/SpriteAnimation.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Animations/SpriteAnimation.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Cameras/Camera.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Cameras/Camera.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Characters/Animal.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Characters/Animal.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Characters/Enemy.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Characters/Enemy.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Characters/Warrior.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Characters/Warrior.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Characters/boss.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Characters/boss.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Collision/CollisionHandler.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Collision/CollisionHandler.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Core/Engine.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Core/Engine.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Graphics/TextureManager.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Graphics/TextureManager.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Inputs/Input.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Inputs/Input.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/MainMenu/GameOver.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/MainMenu/GameOver.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/MainMenu/Menu.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/MainMenu/Menu.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/MainMenu/Text.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/MainMenu/Text.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Maps/MapParser.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Maps/MapParser.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Maps/TileLayer.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Maps/TileLayer.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Replay/Replay.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Replay/Replay.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/SaveAndLoad/saveAndLoad.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/SaveAndLoad/saveAndLoad.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Timer/Timer.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Timer/Timer.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Vendor/tinystr.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Vendor/tinystr.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Vendor/tinyxml.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Vendor/tinyxml.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Vendor/tinyxmlerror.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Vendor/tinyxmlerror.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/Vendor/tinyxmlparser.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/Vendor/tinyxmlparser.cpp.obj"
+  "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/src/main.cpp" "C:/Users/lukah/Desktop/maturitetni_izdelek/3. letnik igrica/cmake-build-debug/CMakeFiles/blojzgame.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
